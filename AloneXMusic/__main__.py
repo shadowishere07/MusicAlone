c5423a5ad66cb927e784188e46e227619d202c80
AkshayXt <130240533+AkshayXt@users.noreply.github.com>
2023-12-16 11:15:29 +0530

    Update __main__.py

--- AloneXMusic/__main__.py
+++ AloneXMusic/__main__.py
@@ -55,7 +55,7 @@ async def init():
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("AloneXMusic").info("Stopping AloneX Music Bot...")
+    LOGGER("AloneXMusic").info("Stopping AloneX Music 🎶 Bot...")
 
 
 if __name__ == "__main__":
